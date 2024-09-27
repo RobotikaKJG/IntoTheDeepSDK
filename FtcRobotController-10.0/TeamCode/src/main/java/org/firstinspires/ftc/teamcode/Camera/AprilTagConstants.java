91a73da8f497a9ab0a72892f1451bb02b5835a6d
Jokpau <jokubassvasiliauskas@gmail.com>
2024-09-27 13:02:02 +0300

    Feat: Make april tag camera intrinsics real-time editable

--- FtcRobotController-10.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Camera/AprilTagConstants.java
+++ FtcRobotController-10.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Camera/AprilTagConstants.java
@@ -1,7 +1,9 @@
 package org.firstinspires.ftc.teamcode.Camera;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 
+@Config
 public class AprilTagConstants {
     public static final double cameraXOffset = 0.196;
     public static final double rightCameraYOffset = 0.07836;
@@ -12,4 +14,9 @@ public class AprilTagConstants {
     public static final Pose2d blueAprilTagPose1 = new Pose2d(60, 41.5, Math.toRadians(0));
     public static final Pose2d blueAprilTagPose2 = new Pose2d(60, 35, Math.toRadians(180));
     public static final Pose2d blueAprilTagPose3 = new Pose2d(60, 28, Math.toRadians(0));
+    public static double fx = 578.272;     // Focal length in x direction (pixels)
+    public static double fy = 578.272;     // Focal length in y direction (pixels)
+    public static double cx = 402.145;     // Optical center x (pixels)
+    public static double cy = 221.506;     // Optical center y (pixels)
+
 }
