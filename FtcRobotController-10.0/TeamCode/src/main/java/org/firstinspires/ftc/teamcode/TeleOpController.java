0019b3849d5a8706a1e992e4981afd8d2070b75f
Benas <BenGren777@gmail.com>
2024-10-09 18:00:51 +0300

    Add imu and loop time

--- FtcRobotController-10.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpController.java
+++ FtcRobotController-10.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpController.java
@@ -4,9 +4,10 @@ import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.IMU;
-
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
 
 @TeleOp
 public class TeleOpController extends LinearOpMode {
@@ -18,7 +19,16 @@ public class TeleOpController extends LinearOpMode {
         DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
         DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
         DcMotor backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
+        IMU imu = hardwareMap.get(IMU.class, "imu");
 
+
+        frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
+        imu.initialize(parameters);
 
         waitForStart();
 
@@ -28,18 +38,35 @@ public class TeleOpController extends LinearOpMode {
             double ly = -gamepad1.left_stick_y;
             double lx = gamepad1.left_stick_x * 1.1;
             double rx = gamepad1.right_stick_x;
+            double startStopwatch = System.nanoTime();
 
+            if (gamepad1.options) {
+                imu.resetYaw();
+            }
 
-            double denominator = Math.max(Math.abs(ly) + Math.abs(lx) + Math.abs(rx), 1);
-            double frontLeftPower = (ly + lx + rx) / denominator;
-            double backLeftPower = (ly - lx + rx) / denominator;
-            double frontRightPower = (ly - lx - rx) / denominator;
-            double backRightPower = (ly + lx - rx) / denominator;
+            double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+
+            double rotX = lx * Math.cos(-botHeading) - ly * Math.sin(-botHeading);
+            double rotY = lx * Math.sin(-botHeading) + ly * Math.cos(-botHeading);
+
+            rotX = rotX * 1.1;  // Counteract imperfect strafing
+
+            // Denominator is the largest motor power (absolute value) or 1
+            double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+            double frontLeftPower = (rotY + rotX + rx) / denominator;
+            double backLeftPower = (rotY - rotX + rx) / denominator;
+            double frontRightPower = (rotY - rotX - rx) / denominator;
+            double backRightPower = (rotY + rotX - rx) / denominator;
 
             frontLeftMotor.setPower(frontLeftPower);
             backLeftMotor.setPower(backLeftPower);
             frontRightMotor.setPower(frontRightPower);
             backRightMotor.setPower(backRightPower);
+
+            // Print out loop time
+            double loopTime = (System.nanoTime() - startStopwatch) / 1000000;
+            telemetry.addData("Loop time:", loopTime);
+            telemetry.update();
         }
 
     }
