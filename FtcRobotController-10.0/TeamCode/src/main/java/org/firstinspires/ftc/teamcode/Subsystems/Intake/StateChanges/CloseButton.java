29b485331c90fec4f498f1aab2135e127156e485
XxXTuk-TukXxX <rojuksrojus@gmail.com>
2024-10-30 16:33:09 +0200

    Feat: Implement CloseButton intake end case

--- FtcRobotController-10.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Intake/StateChanges/CloseButton.java
+++ FtcRobotController-10.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Intake/StateChanges/CloseButton.java
@@ -1,18 +1,57 @@
 package org.firstinspires.ftc.teamcode.Subsystems.Intake.StateChanges;
 
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.Enums.SubsystemState;
+import org.firstinspires.ftc.teamcode.HardwareInterface.EdgeDetection;
+import org.firstinspires.ftc.teamcode.HardwareInterface.MotorConstants;
+import org.firstinspires.ftc.teamcode.HardwareInterface.MotorControl;
+import org.firstinspires.ftc.teamcode.HardwareInterface.SensorControl;
+import org.firstinspires.ftc.teamcode.HardwareInterface.ServoControl;
+import org.firstinspires.ftc.teamcode.HardwareInterface.SlideLogic;
+import org.firstinspires.ftc.teamcode.Subsystems.Intake.IntakeConstants;
+import org.firstinspires.ftc.teamcode.Subsystems.Intake.IntakeController;
+
 public class CloseButton implements IntakeStateChange {
+
+
+    private final SlideLogic slideLogic;
+    private final MotorControl motorControl;
+    private final IntakeController intakeController;
+    private final ElapsedTime elapsedTime;
+    private final EdgeDetection edgeDetection;
+    private double currentWait = 0;
+
+    public CloseButton(SlideLogic slideLogic, IntakeController intakeController, ElapsedTime elapsedTime, MotorControl motorControl, EdgeDetection edgeDetection) {
+        this.slideLogic = slideLogic;
+        this.intakeController = intakeController;
+        this.elapsedTime = elapsedTime;
+        this.motorControl = motorControl;
+        this.edgeDetection = edgeDetection;
+    }
+
     @Override
     public boolean shouldBeStopping() {
-        return false;
+        return edgeDetection.rising(IntakeConstants.closeButton);
     }
 
     @Override
     public void initialiseStop() {
-
+        slideLogic.setSlideExtensionTarget(0);
+        motorControl.setMotorSpeed(MotorConstants.intake, -IntakeConstants.intakeSpeed);
+        addWaitTime(IntakeConstants.intakePushoutTime);
     }
 
     @Override
     public void stop() {
+        slideLogic.updateSlides();
+        if(!slideLogic.slidesBottomReached() || (currentWait > elapsedTime.seconds())) return;
 
+        motorControl.setMotorSpeed(MotorConstants.intake, 0);
+        intakeController.setIntakeState(SubsystemState.Idle);
+    }
+
+    private void addWaitTime(double waitTime) {
+        currentWait = elapsedTime.seconds() + waitTime;
     }
 }
