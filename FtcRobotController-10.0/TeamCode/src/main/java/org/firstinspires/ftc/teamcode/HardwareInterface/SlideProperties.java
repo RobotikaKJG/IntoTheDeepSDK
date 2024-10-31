d7303b0149b38eedce9d60964e19c3f3eef02bee
XxXTuk-TukXxX <rojuksrojus@gmail.com>
2024-10-31 19:15:56 +0200

    Refactor: Remove old proportional code from SlideProperties

--- FtcRobotController-10.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareInterface/SlideProperties.java
+++ FtcRobotController-10.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareInterface/SlideProperties.java
@@ -1,11 +1,8 @@
 package org.firstinspires.ftc.teamcode.HardwareInterface;
 
 public interface SlideProperties {
-    double getSlideKP();
-    double getSlideHoldKP();
     int getSlideMaxExtension();
     double getSlideMovementMaxSpeed();
-    int getSlideHoldThreshold();
     int getSlideExtensionStep();
 
     void setSlideMaxSpeed(double slideMaxSpeed);
--- FtcRobotController-10.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Intake/IntakeSlideProperties.java
+++ FtcRobotController-10.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Intake/IntakeSlideProperties.java
@@ -4,22 +4,11 @@ import org.firstinspires.ftc.teamcode.HardwareInterface.SlideProperties;
 
 public class IntakeSlideProperties implements SlideProperties {
 
-    private double slideMaxSpeed;
-    @Override
-    public double getSlideKP() {
-        //proportional gain
-        return 0.001;
-    }
-
-    @Override
-    public double getSlideHoldKP() {
-        //slide hold proportional gain
-        return 0.001;
-    }
+    private double slideMaxSpeed = 1;
 
     @Override
     public int getSlideMaxExtension() {
-        return 3710;
+        return 2200;
     }
 
     @Override
@@ -28,14 +17,8 @@ public class IntakeSlideProperties implements SlideProperties {
     }
 
     @Override
-    public int getSlideHoldThreshold() {
-        //hold controller active below this encoder distance to target
-        return 100;
-    }
-
-    @Override
     public int getSlideExtensionStep() {
-        return 370;
+        return 550;
     }
 
     @Override
