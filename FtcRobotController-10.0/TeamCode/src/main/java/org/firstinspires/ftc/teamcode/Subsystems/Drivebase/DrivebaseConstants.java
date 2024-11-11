bb52052c7807e3a4478c5585d0c07cb11ec42940
Jokpau <jokubassvasiliauskas@gmail.com>
2024-11-11 18:03:16 +0200

    Refactor: Remove unused imports from DrivebaseConstants

--- FtcRobotController-10.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Drivebase/DrivebaseConstants.java
+++ FtcRobotController-10.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Drivebase/DrivebaseConstants.java
@@ -1,8 +1,5 @@
 package org.firstinspires.ftc.teamcode.Subsystems.Drivebase;
 
-import org.firstinspires.ftc.teamcode.Enums.GamepadIndexValues;
-import org.firstinspires.ftc.teamcode.Subsystems.SubsystemTrigger;
-
 public class DrivebaseConstants{
     public static final double driveSpeed = 1;
     public static final double outtakeModeDriveSpeed = 0.5;
