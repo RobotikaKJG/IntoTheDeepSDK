564eff4833fc37d3527541a14c48160d33676efa
Jokpau <jokubassvasiliauskas@gmail.com>
2024-11-11 17:57:50 +0200

    Fix: Update localization parameters due to changes in hardware

--- FtcRobotController-10.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Roadrunner/StandardTrackingWheelLocalizer.java
+++ FtcRobotController-10.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Roadrunner/StandardTrackingWheelLocalizer.java
@@ -33,8 +33,8 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
     public static double WHEEL_RADIUS = 0.945; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (encoder) speed
 
-    public static double LATERAL_DISTANCE = 6.2912; // in; distance between the left and right wheels
-    public static double FORWARD_OFFSET = -7.1; // in; offset of the lateral wheel
+    public static double LATERAL_DISTANCE = 10.23622; // in; distance between the left and right wheels
+    public static double FORWARD_OFFSET = -4.606299; // in; offset of the lateral wheel
 
     public static double LATERAL_OFFSET = 1.0;
 
@@ -63,10 +63,10 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
 
         leftEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "frontLeftMotor"));
         rightEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "frontRightMotor"));
-        frontEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "backRightMotor"));
+        frontEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "backLeftMotor"));
 
         // TODO: reverse any encoders using Encoder.setDirection(Encoder.Direction.REVERSE)
-        leftEncoder.setDirection(Encoder.Direction.REVERSE);
+        rightEncoder.setDirection(Encoder.Direction.REVERSE);
         //frontEncoder.setDirection(Encoder.Direction.REVERSE);
     }
 
