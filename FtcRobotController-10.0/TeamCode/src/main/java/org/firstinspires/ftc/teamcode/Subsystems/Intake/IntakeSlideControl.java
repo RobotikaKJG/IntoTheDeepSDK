728d77e6832a309c13d20db62171e90729453546
Jokpau <jokubassvasiliauskas@gmail.com>
2024-11-12 18:08:51 +0200

    Feat: Implement limit switch in IntakeSlideControl as future-proofing

--- FtcRobotController-10.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Intake/IntakeSlideControl.java
+++ FtcRobotController-10.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Intake/IntakeSlideControl.java
@@ -2,16 +2,20 @@ package org.firstinspires.ftc.teamcode.Subsystems.Intake;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+import org.firstinspires.ftc.teamcode.HardwareInterface.LimitSwitches;
 import org.firstinspires.ftc.teamcode.HardwareInterface.MotorConstants;
 import org.firstinspires.ftc.teamcode.HardwareInterface.MotorControl;
+import org.firstinspires.ftc.teamcode.HardwareInterface.SensorControl;
 import org.firstinspires.ftc.teamcode.HardwareInterface.SlideControl;
 
 public class IntakeSlideControl implements SlideControl {
     private final MotorControl motorControl;
     private final IntakeSlideProperties intakeSlideProperties = new IntakeSlideProperties();
+    private final SensorControl sensorControl;
 
-    public IntakeSlideControl(MotorControl motorControl) {
+    public IntakeSlideControl(MotorControl motorControl, SensorControl sensorControl) {
         this.motorControl = motorControl;
+        this.sensorControl = sensorControl;
     }
     @Override
     public int getSlidePosition() {
@@ -34,5 +38,10 @@ public class IntakeSlideControl implements SlideControl {
     @Override
     public void resetEncoders() {
         motorControl.resetMotorEncoders(MotorConstants.extendo);
+    }
+
+    @Override
+    public boolean isLimitSwitchPressed() {
+        return sensorControl.isLimitSwitchPressed(LimitSwitches.extendo);
     }
 }
