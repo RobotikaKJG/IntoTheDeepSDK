4944dd433f1120608402e4db0d7ccda0592bd35d
Jokpau <jokubassvasiliauskas@gmail.com>
2024-11-12 18:09:45 +0200

    Feat: Write a test for SensorControl.isColor

--- FtcRobotController-10.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SensorCode/RevColorV2Test.java
+++ FtcRobotController-10.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SensorCode/RevColorV2Test.java
@@ -1,8 +1,13 @@
 package org.firstinspires.ftc.teamcode.SensorCode;
 
+import android.graphics.Color;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.HardwareInterface.SensorControl;
+import org.firstinspires.ftc.teamcode.Roadrunner.StandardTrackingWheelLocalizer;
 
 @TeleOp
 public class RevColorV2Test extends LinearOpMode {
@@ -13,6 +18,9 @@ public class RevColorV2Test extends LinearOpMode {
     public void runOpMode() {
         // Get the color sensor from hardwareMap
         color = hardwareMap.get(ColorSensor.class, "ColorSensor");
+        SensorControl sensorControl = new SensorControl(hardwareMap,gamepad1,new StandardTrackingWheelLocalizer(hardwareMap));
+        int targetColor = Color.rgb(0,0,0);
+        int threshold = 10;
 
         // Wait for the Play button to be pressed
         waitForStart();
@@ -22,6 +30,7 @@ public class RevColorV2Test extends LinearOpMode {
             telemetry.addData("Red", color.red());
             telemetry.addData("Green", color.green());
             telemetry.addData("Blue", color.blue());
+            telemetry.addData("IsColor:",sensorControl.isColorMatch(targetColor,threshold));
             telemetry.update();
         }
     }
