8805808d75b095fe07223742cf8068dbee8e134d
Jokpau <jokubassvasiliauskas@gmail.com>
2025-01-02 17:36:34 +0200

    Feat: Implement Drivebase-only teleop for testing

---
+++ FtcRobotController-10.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main/ManualOpModes/DrivebaseTest.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode.Main.ManualOpModes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.HardwareInterface.Motor.MotorConstants;
+import org.firstinspires.ftc.teamcode.Main.Dependencies;
+import org.firstinspires.ftc.teamcode.Main.GlobalVariables;
+import org.firstinspires.ftc.teamcode.Subsystems.Drivebase.DrivebaseController;
+
+@TeleOp
+public class DrivebaseTest extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        GlobalVariables.isAutonomous = false;
+        Dependencies dependencies = new Dependencies(hardwareMap, gamepad1,gamepad2, telemetry);
+        DrivebaseController drivebaseController = dependencies.createDrivebaseController();
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+            drivebaseController.updateState();
+            dependencies.motorControl.setMotors(MotorConstants.allDrive);
+            telemetry.update();
+        }
+    }
+}
