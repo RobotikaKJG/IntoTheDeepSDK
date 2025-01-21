cf1c5941de9d2d95ca81b5af40260439a9619a56
jokpau <jokubassvasiliauskas@gmail.com>
2025-01-21 20:07:09 +0200

    Fix: Fix outtake arm servo constants due to hardware changes

--- FtcRobotController-10.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Outtake/OuttakeConstants.java
+++ FtcRobotController-10.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Outtake/OuttakeConstants.java
@@ -1,15 +1,13 @@
 package org.firstinspires.ftc.teamcode.Subsystems.Outtake;
 
 public class OuttakeConstants {
-    public static final double outtakeLeftServoMaxPos = 0.85;//0.965;//+0.0?     // Placing position,
+    public static final double outtakeLeftServoMaxPos = 0.88;//0.965;//+0.0?     // Placing position,
     public static final double outtakeLeftServoMinPos = 0.1675;//0.2;     // Taking position, starting position
     public static final double releaseServoMaxPos = 0.77;     // Maximum rotational position, claw open
     public static final double releaseServoReleasePos = 0.60;     // used for upOpen
     public static final double releaseServoMinPos = 0.45;     // Minimum rotational position, claw closed position
     public static final double outtakeArmWait = 0.8;
-    public static final double outtakeArmCloseWait = 1;//0.65;
-    public static final int lowBasketPos = 1350;
-    public static final int highBasketPos = 2000;
+    public static final double outtakeArmCloseWait = 0.8;//0.65;
     public static final int lowBasketPos = 1100;
     public static final int highBasketPos = 2100;
     public static final int slideTargetThreshold = 20;
