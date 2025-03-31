89a2735b05db2bef28ff60bc75f5b751184e6a4d
jokpau <jokubassvasiliauskas@gmail.com>
2025-03-31 08:28:27 +0300

    Fix: Reduce sample claw max position to fit in intake

--- FtcRobotController-10.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Outtake/OuttakeConstants.java
+++ FtcRobotController-10.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Outtake/OuttakeConstants.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode.Subsystems.Outtake;
 public class OuttakeConstants {
     public static final double outtakeLeftServoMaxPos = 0.825;//0.82;//0.88;//0.965;//+0.0?     // Taking position, starting position
     public static final double outtakeLeftServoMinPos = 0.225;//0.065;//1675;//0.2;     // Placing position,
-    public static final double releaseServoMaxPos = 0.80;     // Maximum rotational position, claw open
+    public static final double releaseServoMaxPos = 0.70;     // Maximum rotational position, claw open
     public static final double releaseServoReleasePos = 0.65;     // used for upOpen
     public static final double releaseServoMinPos = 0.48;     // Minimum rotational position, claw closed position
     public static final double outtakeArmWait = 0.6;
