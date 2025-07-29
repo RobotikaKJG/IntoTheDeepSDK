f6719bac27f4de32bd017af625a4a49e71eb0afb
jokpau <jokubassvasiliauskas@gmail.com>
2025-07-29 13:05:12 +0300

    Feat: Implement manual hang control

---
+++ FtcRobotController-10.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main/ManualOpModes/HangConstants.java
@@ -0,0 +1,5 @@
+package org.firstinspires.ftc.teamcode.Main.ManualOpModes;
+
+public class HangConstants {
+    public static double motorSpeed = 0.2;
+}
---
+++ FtcRobotController-10.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main/ManualOpModes/ManualHangControl.java
@@ -0,0 +1,76 @@
+package org.firstinspires.ftc.teamcode.Main.ManualOpModes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+import org.firstinspires.ftc.teamcode.HardwareInterface.Gamepad.GamepadIndexValues;
+import org.firstinspires.ftc.teamcode.HardwareInterface.Motor.MotorConstants;
+import org.firstinspires.ftc.teamcode.HardwareInterface.Sensor.LimitSwitches;
+import org.firstinspires.ftc.teamcode.Main.Dependencies;
+import org.firstinspires.ftc.teamcode.Main.GlobalVariables;
+import org.firstinspires.ftc.teamcode.Subsystems.Outtake.Slides.OuttakeSlideControl;
+
+@TeleOp
+public class ManualHangControl extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        GlobalVariables.isAutonomous = false;
+        Dependencies dependencies = new Dependencies(hardwareMap, gamepad1,gamepad2, telemetry);
+        OuttakeSlideControl outtakeSlideControl = new OuttakeSlideControl(dependencies.motorControl,dependencies.sensorControl);
+        int slidePosition = 0;
+        Gamepad currentGamepad1 = new Gamepad();
+        Gamepad prevGamepad1 = new Gamepad();
+        prevGamepad1.copy(currentGamepad1);
+        currentGamepad1.copy(gamepad1);
+        waitForStart();
+        boolean retracting = true;
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+
+            prevGamepad1.copy(currentGamepad1);
+            currentGamepad1.copy(gamepad1);
+            if(gamepad1.triangle) break;
+            dependencies.edgeDetection.refreshGamepadIndex(currentGamepad1,prevGamepad1);
+            telemetry.addLine("Press square to extend, press circle to retract");
+            telemetry.addData("Slide position", outtakeSlideControl.getSlidePosition());
+            telemetry.addData("Retracting", retracting);
+            telemetry.addData("isPressed",dependencies.sensorControl.isLimitSwitchPressed(LimitSwitches.slide));
+            telemetry.addData("position",dependencies.motorControl.getMotorPosition(MotorConstants.bothSlides));
+            telemetry.update();
+            if(dependencies.edgeDetection.rising(GamepadIndexValues.circle))
+            {
+                slidePosition -= 50;
+                outtakeSlideControl.setSlidePosition(slidePosition);
+            }
+            if(dependencies.edgeDetection.rising(GamepadIndexValues.square))
+            {
+                slidePosition += 50;
+                outtakeSlideControl.setSlidePosition(slidePosition);
+            }
+
+            if(dependencies.edgeDetection.rising(GamepadIndexValues.dpadUp))
+            {
+                outtakeSlideControl.setSlidePosition(900);
+            }
+            if(dependencies.edgeDetection.rising(GamepadIndexValues.dpadDown))
+            {
+                outtakeSlideControl.setSlidePosition(0);
+                retracting = true;
+            }
+
+            if(gamepad1.right_trigger > 0) {
+                dependencies.motorControl.setMotorSpeed(MotorConstants.frontLeft, HangConstants.motorSpeed);
+                dependencies.motorControl.setMotorSpeed(MotorConstants.frontRight, HangConstants.motorSpeed);
+
+            }
+            else if(gamepad1.left_trigger > 0) {
+                dependencies.motorControl.setMotorSpeed(MotorConstants.frontLeft, -HangConstants.motorSpeed);
+                dependencies.motorControl.setMotorSpeed(MotorConstants.frontRight, -HangConstants.motorSpeed);
+            }
+        }
+    }
+}
