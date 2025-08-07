cdd805a60f5eb8085919489e4e1c95fe21da1675
BenGren <BenGren777@gmail.com>
2025-08-07 14:52:53 +0300

    Outtake servos test

--- FtcRobotController-10.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareInterface/Servo/AxonMaxServoTest.java
+++ FtcRobotController-10.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareInterface/Servo/AxonMaxServoTest.java
@@ -15,22 +15,26 @@ public class AxonMaxServoTest extends LinearOpMode {
         axonServoOne = hardwareMap.get(Servo.class, "axonServoOne");
         axonServoTwo = hardwareMap.get(Servo.class, "axonServoTwo");
 
-        // Map 150 degrees to servo position (assuming 0-300 deg -> 0.0-1.0)
-        double degrees = 150.0;
-        double position = degrees / 255.0;
+//        // Map 150 degrees to servo position (assuming 0-300 deg -> 0.0-1.0)
+//        double degrees = 150.0;
+//        double position = degrees / 255.0;
 
-        telemetry.addLine("Press A to move servo to 150 degrees");
+//        telemetry.addLine("Press A to move servo to 150 degrees");
         telemetry.update();
 
         waitForStart();
 
         while (opModeIsActive()) {
             if (gamepad1.a) {
-                axonServoOne.setPosition(position);
-                axonServoTwo.setPosition(position);
-                telemetry.addData("Commanded Position (deg)", degrees);
-                telemetry.addData("Set Position (0.0-1.0)", position);
-                telemetry.update();
+                axonServoOne.setPosition(1);
+                axonServoTwo.setPosition(0.97);
+//                telemetry.addData("Commanded Position (deg)", degrees);
+//                telemetry.addData("Set Position (0.0-1.0)", position);
+//                telemetry.update();
+            }
+            else {
+                axonServoOne.setPosition(0.02);
+                axonServoTwo.setPosition(0.0);
             }
         }
     }
