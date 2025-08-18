ebda6a13eb630dc990d00b4f1ddfe32c725fe465
jokpau <jokubassvasiliauskas@gmail.com>
2025-08-18 21:46:45 +0300

    Fix: Update values in color test to match real values

--- FtcRobotController-10.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main/ManualOpModes/RevColorV2Test.java
+++ FtcRobotController-10.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main/ManualOpModes/RevColorV2Test.java
@@ -80,16 +80,16 @@ public class RevColorV2Test extends LinearOpMode {
 
     public boolean isRed(){
         //return currentGreen < 5 && currentRed > 7 || (currentBlue == 2 && currentGreen == 2 && currentRed == 5);
-        return currentGreen < 14 && currentRed > 12;
+        return currentGreen < 10 && currentRed > 8;
     }
 
     public boolean isYellow(){
-        return currentGreen > 13;
+        return currentGreen > 10;
     }
 
     public boolean isBlue(){
 //        return (currentRed < 5 && currentBlue > 3 && currentGreen < 8) || ( currentRed == 1 && currentBlue == 3 && currentGreen < 4);
-        return  currentRed < 10;// && currentBlue >= 6;
+        return  currentRed < 10 && currentBlue > 6;
     }
 
     public double getDistance(){
